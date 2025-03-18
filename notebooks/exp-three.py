45780ba336aecac72fe80e408810a3b94ea56c5f
Neeraj Bhauryal <neeraj.bhauryal@gmail.com>
2025-03-18 18:29:35 +0100

    Experiments done

---
+++ notebooks/exp-three.py
@@ -0,0 +1,161 @@
+import os
+import re
+import string
+import numpy as np
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+import dagshub
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.preprocessing import LabelEncoder
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+warnings.filterwarnings("ignore")
+
+# Set MLflow Tracking URI & DAGsHub integration
+MLFLOW_TRACKING_URI = "https://dagshub.com/bhauryal7/NewsClassification.mlflow"
+dagshub.init(repo_owner="bhauryal7", repo_name="NewsClassification", mlflow=True)
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+mlflow.set_experiment("LR Hyperparameter Tuning")
+
+
+# ========================== TEXT PREPROCESSING ===============================
+# Define text preprocessing functions
+def lemmatization(text):
+    """Lemmatize the text."""
+    lemmatizer = WordNetLemmatizer()
+    text = text.split()
+    text = [lemmatizer.lemmatize(word) for word in text]
+    return " ".join(text)
+
+def remove_stop_words(text):
+    """Remove stop words from the text."""
+    stop_words = set(stopwords.words("english"))
+    text = [word for word in str(text).split() if word not in stop_words]
+    return " ".join(text)
+
+def removing_numbers(text):
+    """Remove numbers from the text."""
+    text = ''.join([char for char in text if not char.isdigit()])
+    return text
+
+def lower_case(text):
+    """Convert text to lower case."""
+    text = text.split()
+    text = [word.lower() for word in text]
+    return " ".join(text)
+
+def removing_punctuations(text):
+    """Remove punctuations from the text."""
+    text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+    text = text.replace('؛', "")
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
+
+def removing_urls(text):
+    """Remove URLs from the text."""
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    return url_pattern.sub(r'', text)
+
+def normalize_text(df):
+    """Normalize the text data."""
+    try:
+        df['title'] = df['title'].apply(lower_case)
+        df['title'] = df['title'].apply(remove_stop_words)
+        df['title'] = df['title'].apply(removing_numbers)
+        df['title'] = df['title'].apply(removing_punctuations)
+        df['title'] = df['title'].apply(removing_urls)
+        df['title'] = df['title'].apply(lemmatization)
+        return df
+    except Exception as e:
+        print(f'Error during text normalization: {e}')
+        raise 
+
+# ==========================
+# Load & Prepare Data
+# ==========================
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+vectorizer = TfidfVectorizer()
+label_encoder = LabelEncoder()
+
+def load_and_prepare_data(filepath):
+    """Loads, preprocesses, and vectorizes the dataset."""
+    df = pd.read_csv(filepath)
+    df = normalize_text(df)
+    df["topic"] = label_encoder.fit_transform(df["topic"])
+    X = vectorizer.fit_transform(df["title"])
+    y = df["topic"]
+    
+    return train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# ==========================
+# Train & Log Model
+# ==========================
+def train_and_log_model(X_train, X_test, y_train, y_test):
+    """Trains a Stochasti Gradient model with GridSearch and logs results to MLflow."""
+    
+    param_grid = {
+    #'loss': ['hinge', 'log_loss', 'modified_huber'],  # SVM, logistic regression, and robust loss
+    'penalty': ['l1', 'l2', 'elasticnet'],  # Regularization methods
+    'alpha': [1e-4, 1e-3, 1e-2],  # Regularization strength
+    #'learning_rate': ['optimal', 'constant', 'invscaling', 'adaptive'],  # Learning rate strategies
+    #'eta0': [0.001, 0.01, 0.1],  # Initial learning rate (only for 'constant' learning rate)
+    #'early_stopping': [True, False],  # Whether to use early stopping
+    #'tol': [1e-3, 1e-4, 1e-5],  # Stopping tolerance
+}
+
+    
+    with mlflow.start_run():
+        grid_search = GridSearchCV(SGDClassifier(max_iter=10000), param_grid, cv=5, scoring="accuracy", n_jobs=-1)
+        grid_search.fit(X_train, y_train)
+
+        # Log all hyperparameter tuning runs
+        for params, mean_score, std_score in zip(grid_search.cv_results_["params"], 
+                                                 grid_search.cv_results_["mean_test_score"], 
+                                                 grid_search.cv_results_["std_test_score"]):
+            with mlflow.start_run(run_name=f"LR with params: {params}", nested=True):
+                model = SGDClassifier(**params)
+                model.fit(X_train, y_train)
+                
+                y_pred = model.predict(X_test)
+                
+                metrics = {
+                    "accuracy": accuracy_score(y_test, y_pred),
+                    "precision": precision_score(y_test, y_pred,average='weighted'),
+                    "recall": recall_score(y_test, y_pred,average='weighted'),
+                    "f1_score": f1_score(y_test, y_pred,average='weighted'),
+                    "mean_cv_score": mean_score,
+                    "std_cv_score": std_score
+                }
+                
+                # Log parameters & metrics
+                mlflow.log_params(params)
+                mlflow.log_metrics(metrics)
+                
+                print(f"Params: {params} | Accuracy: {metrics['accuracy']:.4f} | F1: {metrics['f1_score']:.4f}")
+
+        # Log the best model
+        best_params = grid_search.best_params_
+        best_model = grid_search.best_estimator_
+        best_accuracy = grid_search.best_score_
+
+        mlflow.log_params(best_params)
+        mlflow.log_metric("accuracy", best_accuracy )
+        mlflow.sklearn.log_model(best_model, "model")
+        
+        print(f"\nBest Params: {best_params} | Best Accuracy: {best_accuracy:.4f}")
+
+
+# ==========================
+# Main Execution
+# ==========================
+if __name__ == "__main__":
+    (X_train, X_test, y_train, y_test) = load_and_prepare_data("notebooks/sample.csv")
+    train_and_log_model(X_train, X_test, y_train, y_test)
---
+++ notebooks/exp-two-bow-vs-tfidf.py
@@ -0,0 +1,202 @@
+import setuptools
+import os
+import re
+import string
+import pandas as pd
+pd.set_option('future.no_silent_downcasting', True)
+
+import numpy as np
+import mlflow
+import mlflow.sklearn
+import dagshub
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.naive_bayes import MultinomialNB
+from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import scipy.sparse
+
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+warnings.filterwarnings("ignore")
+
+# ========================== CONFIGURATION ==================================
+CONFIG = {
+    "data_path": "notebooks/sample.csv",
+    "test_size": 0.2,
+    "mlflow_tracking_uri": "https://dagshub.com/bhauryal7/NewsClassification.mlflow",
+    "dagshub_repo_owner": "bhauryal7",
+    "dagshub_repo_name": "NewsClassification",
+    "experiment_name": "BOW vs TFIDF"
+}
+
+# ========================== SETUP MLflow & DAGSHUB ==========================
+mlflow.set_tracking_uri(CONFIG["mlflow_tracking_uri"])
+dagshub.init(repo_owner=CONFIG["dagshub_repo_owner"], repo_name=CONFIG["dagshub_repo_name"], mlflow=True)
+mlflow.set_experiment(CONFIG["experiment_name"])
+
+# ========================== TEXT PREPROCESSING ===============================
+# Define text preprocessing functions
+def lemmatization(text):
+    """Lemmatize the text."""
+    lemmatizer = WordNetLemmatizer()
+    text = text.split()
+    text = [lemmatizer.lemmatize(word) for word in text]
+    return " ".join(text)
+
+def remove_stop_words(text):
+    """Remove stop words from the text."""
+    stop_words = set(stopwords.words("english"))
+    text = [word for word in str(text).split() if word not in stop_words]
+    return " ".join(text)
+
+def removing_numbers(text):
+    """Remove numbers from the text."""
+    text = ''.join([char for char in text if not char.isdigit()])
+    return text
+
+def lower_case(text):
+    """Convert text to lower case."""
+    text = text.split()
+    text = [word.lower() for word in text]
+    return " ".join(text)
+
+def removing_punctuations(text):
+    """Remove punctuations from the text."""
+    text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+    text = text.replace('؛', "")
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
+
+def removing_urls(text):
+    """Remove URLs from the text."""
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    return url_pattern.sub(r'', text)
+
+def normalize_text(df):
+    """Normalize the text data."""
+    try:
+        df['title'] = df['title'].apply(lower_case)
+        df['title'] = df['title'].apply(remove_stop_words)
+        df['title'] = df['title'].apply(removing_numbers)
+        df['title'] = df['title'].apply(removing_punctuations)
+        df['title'] = df['title'].apply(removing_urls)
+        df['title'] = df['title'].apply(lemmatization)
+        return df
+    except Exception as e:
+        print(f'Error during text normalization: {e}')
+        raise 
+
+# ========================== LOAD & PREPROCESS DATA ==========================
+label_encoder = LabelEncoder()
+def load_data(file_path):
+    try:
+        df = pd.read_csv(file_path)
+        df = normalize_text(df)
+        df["topic"] = label_encoder.fit_transform(df["topic"])
+        return df
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        raise
+
+# ========================== FEATURE ENGINEERING ==========================
+VECTORIZERS = {
+    'BoW': CountVectorizer(),
+    'TF-IDF': TfidfVectorizer()
+}
+
+ALGORITHMS = {
+    'LogisticRegression': LogisticRegression(),
+    'MultinomialNB': MultinomialNB(),
+    'XGBoost': XGBClassifier(),
+    'RandomForest': RandomForestClassifier(),
+    'GradientBoosting': GradientBoostingClassifier(),
+    'SupportVectorMachine': SVC(),
+    'StochasticGradientDescent': SGDClassifier()
+}
+
+# ========================== TRAIN & EVALUATE MODELS ==========================
+def train_and_evaluate(df):
+    with mlflow.start_run(run_name="All Experiments") as parent_run:
+        for algo_name, algorithm in ALGORITHMS.items():
+            for vec_name, vectorizer in VECTORIZERS.items():
+                with mlflow.start_run(run_name=f"{algo_name} with {vec_name}", nested=True) as child_run:
+                    try:
+                        # Feature extraction
+                        X = vectorizer.fit_transform(df['title'])
+                        y = df['topic']
+                        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=CONFIG["test_size"], random_state=42)
+
+                        # Log preprocessing parameters
+                        mlflow.log_params({
+                            "vectorizer": vec_name,
+                            "algorithm": algo_name,
+                            "test_size": CONFIG["test_size"]
+                        })
+
+                        # Train model
+                        model = algorithm
+                        model.fit(X_train, y_train)
+
+                        # Log model parameters
+                        log_model_params(algo_name, model)
+
+                        # Evaluate model
+                        y_pred = model.predict(X_test)
+                        metrics = {
+                            "accuracy": accuracy_score(y_test, y_pred),
+                            "precision": precision_score(y_test, y_pred,average='weighted'),
+                            "recall": recall_score(y_test, y_pred,average='weighted'),
+                            "f1_score": f1_score(y_test, y_pred,average='weighted')
+                        }
+                        mlflow.log_metrics(metrics)
+
+                        # Log model
+                        # mlflow.sklearn.log_model(model, "model")
+                        input_example = X_test[:5] if not scipy.sparse.issparse(X_test) else X_test[:5].toarray()
+                        mlflow.sklearn.log_model(model, "model", input_example=input_example)
+
+                        # Print results for verification
+                        print(f"\nAlgorithm: {algo_name}, Vectorizer: {vec_name}")
+                        print(f"Metrics: {metrics}")
+
+                    except Exception as e:
+                        print(f"Error in training {algo_name} with {vec_name}: {e}")
+                        mlflow.log_param("error", str(e))
+
+def log_model_params(algo_name, model):
+    """Logs hyperparameters of the trained model to MLflow."""
+    params_to_log = {}
+    if algo_name == 'LogisticRegression':
+        params_to_log["C"] = model.C
+    elif algo_name == 'MultinomialNB':
+        params_to_log["alpha"] = model.alpha
+    elif algo_name == 'XGBoost':
+        params_to_log["n_estimators"] = model.n_estimators
+        params_to_log["learning_rate"] = model.learning_rate
+    elif algo_name == 'RandomForest':
+        params_to_log["n_estimators"] = model.n_estimators
+        params_to_log["max_depth"] = model.max_depth
+    elif algo_name == 'GradientBoosting':
+        params_to_log["n_estimators"] = model.n_estimators
+        params_to_log["learning_rate"] = model.learning_rate
+        params_to_log["max_depth"] = model.max_depth
+    elif algo_name == 'SupportVectorMachine':
+        params_to_log['C'] = model.C
+    elif algo_name =='StochasticGradientDescent':
+        params_to_log['alpha'] = model.alpha        
+
+
+
+    mlflow.log_params(params_to_log)
+
+# ========================== EXECUTION ==========================
+if __name__ == "__main__":
+    df = load_data(CONFIG["data_path"])
+    train_and_evaluate(df)
