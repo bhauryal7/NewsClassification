080cd2428a654ebf48cf32cfae74dd9df2869c07
Neeraj Bhauryal <neeraj.bhauryal@gmail.com>
2025-03-19 23:08:40 +0100

    Model registery done

---
+++ src/data/data_ingestion.py
@@ -0,0 +1,96 @@
+# data ingestion
+import numpy as np
+import pandas as pd
+pd.set_option('future.no_silent_downcasting', True)
+import joblib
+
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import yaml
+import logging
+from src.logger import logging
+from src.connections import s3_connection
+
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logging.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logging.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logging.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logging.error('Unexpected error: %s', e)
+        raise
+
+def load_data(data_url: str) -> pd.DataFrame:
+    """Load data from a CSV file."""
+    try:
+        df = pd.read_csv(data_url)
+        logging.info('Data loaded from %s', data_url)
+        return df
+    except pd.errors.ParserError as e:
+        logging.error('Failed to parse the CSV file: %s', e)
+        raise
+    except Exception as e:
+        logging.error('Unexpected error occurred while loading the data: %s', e)
+        raise
+
+label_encoder = LabelEncoder()
+
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+    """Preprocess the data."""
+    try:
+        logging.info("pre-processing...")
+        df["topic"] = label_encoder.fit_transform(df["topic"])
+        logging.info('Data preprocessing completed')
+        return df
+    except KeyError as e:
+        logging.error('Missing column in the dataframe: %s', e)
+        raise
+    except Exception as e:
+        logging.error('Unexpected error during preprocessing: %s', e)
+        raise
+
+def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str) -> None:
+    """Save the train and test datasets."""
+    try:
+        raw_data_path = os.path.join(data_path, 'raw')
+        os.makedirs(raw_data_path, exist_ok=True)
+        train_data.to_csv(os.path.join(raw_data_path, "train.csv"), index=False)
+        test_data.to_csv(os.path.join(raw_data_path, "test.csv"), index=False)
+        logging.debug('Train and test data saved to %s', raw_data_path)
+    except Exception as e:
+        logging.error('Unexpected error occurred while saving the data: %s', e)
+        raise
+
+def main():
+    try:
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        #test_size = 0.2
+        
+        df = load_data(data_url='./notebooks/sample.csv')
+        #s3 = s3_connection.s3_operations("newsclassify", "AKIA3LET6G5DOK2XDIDS", "xC3Xs/bq1tkPavWt46qmFeMqUotue6G+/vG+Bfcu")
+        #df = s3.fetch_file_from_s3("sample.csv")
+
+
+
+        final_df = preprocess_data(df)
+        train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=42)
+        save_data(train_data, test_data, data_path='./data')
+        # Save the label_encoder to a file
+        joblib.dump(label_encoder, 'models/label_encoder.pkl')
+    except Exception as e:
+        logging.error('Failed to complete the data ingestion process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
